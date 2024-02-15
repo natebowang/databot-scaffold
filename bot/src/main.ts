d9688cc13f4a1a1e212d3ca79aa739c5661121be
Nathan Wang <nathanbotinker@gmail.com>
2024-02-16 08:03:04 +1300

    Add color to message in console log

--- bot/src/main.ts
+++ bot/src/main.ts
@@ -5,8 +5,5 @@ let t = 0
 
 setInterval(() => {
   logger.http(`${t++} Hello World`)
-  logger.warn(`${t++} Hello World`)
-  logger.debug(`${t++} Hello World`)
-  logger.error(`${t++} Hello World`)
-  logger.info(`${t++} Hello World`)
+  logger.info('hello', { obj: 'more', ano: 3, nest: { a: 'seg' } })
 }, 40)
--- bot/src/shared/logger/main.ts
+++ bot/src/shared/logger/main.ts
@@ -7,7 +7,7 @@ export const logger = createLogger({
   transports: [
     new transports.Console({
       format: format.combine(
-        format.colorize(),
+        format.colorize({ all: true }),
         format.timestamp(),
         format.printf((info) => {
           const { timestamp, level, message, ...args } = info
